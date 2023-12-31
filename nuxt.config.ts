8aa0d5c64301d70f9842cdfae4b179dc3678c98a
Diego Castillo <diego.castilloguajardo@gmail.com>
2023-12-31 18:34:24 -0300

    📦 build(npm): Add pinia and @pinia/nuxt dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  modules: [
+    '@pinia/nuxt',
+  ],
   css: ['~/assets/css/main.css', '~/assets/scss/main.scss'],
   vite: {
     css: {
